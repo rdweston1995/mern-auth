d263b6ed40a29982502b9f326941be17995cdf67
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-27 18:05:47 -0700

    Added server file. Init

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+//Bodyparser middleware
+app.user(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("mongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+//Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+//Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
